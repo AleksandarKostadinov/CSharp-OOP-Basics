5f7a5e71418a99400bff105dd4f253a3d4dd3440
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-07-05 11:27:11 +0300

    Added new solution 02.Encapsulation - Lab with Persons.csproj in

---
+++ 02.Encapsulation - Lab/Persons/Person.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.CodeDom;
+
+class Person
+{
+    private string firtstName;
+    private string lastName;
+    private int age;
+    private double salary;
+
+    public string FirstName
+    {
+        get { return this.firtstName; }
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot be less than 3 symbols");
+            }
+
+            this.firtstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get { return this.lastName; }
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot be less than 3 symbols");
+            }
+
+            this.lastName = value;
+        }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{nameof(this.Age)} cannot be zero or negative integer");
+            }
+
+            this.age = value;
+        }
+    }
+
+    public double Salary
+    {
+        get { return this.salary; }
+        set
+        {
+            if (value < 460)
+            {
+                throw new ArgumentException($"{nameof(this.Salary)} cannot be less than 460 leva");
+            }
+
+            this.salary = value;
+        }
+    }
+
+    public Person(string firtstName, string lastName, int age, double salary)
+    {
+        this.FirstName = firtstName;
+        this.LastName = lastName;
+        this.Age = age;
+        this.Salary = salary;
+    }
+
+    public void IncreaseSalary(double percent)
+    {
+        if (this.age > 30)
+        {
+            this.salary += this.salary * percent / 100;
+        }
+        else
+        {
+            this.salary += this.salary * percent / 200;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.firtstName} {this.lastName} get {this.salary:f2} leva";
+    }
+}
---
+++ 02.Encapsulation - Lab/Persons/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var teamName = Console.ReadLine();
+        var lines = int.Parse(Console.ReadLine());
+        var team = new Team(teamName);
+        for (int i = 0; i < lines; i++)
+        {
+            var cmdArgs = Console.ReadLine().Split();
+            try
+            {
+                var person = new Person(cmdArgs[0],
+                    cmdArgs[1],
+                    int.Parse(cmdArgs[2]),
+                    double.Parse(cmdArgs[3]));
+
+                team.AddPlayer(person);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        Console.WriteLine( $"First team have {team.FirstTeam.Count} players");
+        Console.WriteLine( $"Reserve team have {team.ReserveTeam.Count} players");
+    }
+
+}
